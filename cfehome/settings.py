9bd5f89a3516e8abdacf71e88d8500cee38c9c19
prakashranjan <prakashranjansingh04@gmail.com>
2018-08-30 01:59:21 +0530

    django setup complete

--- cfehome/settings.py
+++ cfehome/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'h*gg_z@)v!k^0@5stcuw81rxvtw!g9psf#&a&5do_j!yp!vzfh'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'postings'
 ]
 
 MIDDLEWARE = [
@@ -118,3 +120,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
+}
